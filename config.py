b324ebd854eeac4d7be28b9821fc7d230cdc208f
Juicy2b <104423170+Juicy2b@users.noreply.github.com>
2022-07-04 12:22:47 +0200

    Create config.py

---
+++ config.py
@@ -0,0 +1,10 @@
+# WiFi credentials
+WIFI_SSID = 'X'
+WIFI_PASS = 'Y'
+
+# Datacake credentials
+SERIAL_NUMBER = '187Yuusuf187'
+MQTT_BROKER = 'mqtt.datacake.co'
+TOKEN = '2bd4e0a590914bcb20375d34f39e0b6811eb5acc'
+PORT = 1883
+TOPIC = 'dtck-pub/arduino-5/0828c74a-66f2-4474-941e-c6ca3f28c571/TEMPERATURE'
